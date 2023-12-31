670f02c8004a701144a9ac90b65364f8f1c80272
User <User@DESKTOP-JN9FIUJ>
2023-12-31 15:37:40 +0100

    Add project files.

---
+++ Models/CRUDContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TempTrackApp.Models
+{
+    public partial class CRUDContext : DbContext
+    {
+        public CRUDContext()
+        {
+        }
+
+        public CRUDContext(DbContextOptions<CRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Klienti> Klientis { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Klienti>(entity =>
+            {
+                entity.ToTable("Klienti");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Klienti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TempTrackApp.Models
+{
+    public partial class Klienti
+    {
+        public int Id { get; set; }
+        public int ClientCode { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
